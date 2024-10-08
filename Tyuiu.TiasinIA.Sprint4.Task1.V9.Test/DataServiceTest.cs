9ef1765418d11dcc01c808d1632424784570901e
Илья Тясин <turbopad7200@gmail.com>
2024-10-08 18:29:57 +0500

    Task1

---
+++ Tyuiu.TiasinIA.Sprint4.Task1.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TiasinIA.Sprint4.Task1.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
