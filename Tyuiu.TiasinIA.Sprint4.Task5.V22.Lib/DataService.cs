4b6696ecfd9c0e6b3470439b54cd924143d27fb7
Илья Тясин <turbopad7200@gmail.com>
2024-10-12 10:58:19 +0500

    Task6

---
+++ Tyuiu.TiasinIA.Sprint4.Task5.V22.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TiasinIA.Sprint4.Task5.V22.Lib
+{
+    public class DataService : ISprint4Task5V22
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int colums = matrix.GetUpperBound(0) + 1;
+            int rows = matrix.Length / colums;
+
+
+            int count = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.TiasinIA.Sprint4.Task5.V22/Program.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.TiasinIA.Sprint4.Task5.V22.Lib;
+namespace Tyuiu.TiasinIA.Sprint4.Task5.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Тясин И. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Тясин Илья Александрович | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от -8 до 9.             *");
+            Console.WriteLine("* Найти количество отрицательных элементов                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int colums = Convert.ToInt32(Console.ReadLine());
+
+            int[,] ntrx = new int[rows, colums];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.WriteLine($"Введите {i}, {j} элемент массива: ");
+                    ntrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{ntrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(ntrx);
+
+            
+            Console.Write(res);
+
+            Console.ReadLine();
+        }
+    }
+}
